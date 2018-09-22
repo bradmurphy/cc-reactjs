531a2b00dc103692c23f81d27d88a74bab079c3f
Brad Murphy <bm.dev84@gmail.com>
2018-09-22 16:20:16 -0400

    Basic routing.

---
+++ master/duke-cannon/src/components/app/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './app.css';
+
+import Home from '../home';
+import Shop from '../shop';
+import Order from '../order';
+import Account from '../account';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <nav className="App-nav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/shop">Shop</Link>
+              </li>
+              <li>
+                <Link to="/order">Order</Link>
+              </li>
+              <li>
+                <Link to="/account">Account</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/order" component={Order} />
+          <Route path="/account" component={Account} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ master/duke-cannon/src/components/shop/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+  render() {
+    return <h1>Shop</h1>;
+  }
+}
+
+export default Shop;
