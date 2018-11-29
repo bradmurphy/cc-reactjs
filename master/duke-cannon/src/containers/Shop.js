1fd1e2713f79c1ca2d38554fdf28aae7588f77ce
Brad Murphy <brad.murphy@Brads-MacBook-Pro.local>
2018-11-29 13:12:43 -0500

    adding categories.

--- master/duke-cannon/src/containers/Shop.js
+++ master/duke-cannon/src/containers/Shop.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Product from '../components/Product';
 
-const Shop = ({ items, add }) => (
-  <div>
-    {items.map((item, index) => {
-      return <Product key={index} item={item} add={add} />;
-    })}
-  </div>
-);
+class Shop extends Component {
+  state = {
+    items: this.props.items,
+  };
+
+  filterCategory(category) {
+    const { items } = this.props;
+    const filteredCategory =
+      category === 'All' ? items : items.filter(item => item.category === category);
+    this.setState({ items: filteredCategory });
+  }
+
+  render() {
+    const { items } = this.state;
+    const { add } = this.props;
+
+    return (
+      <section>
+        <nav>
+          <span className="category-link" onClick={() => this.filterCategory('All')}>
+            ALL
+          </span>{' '}
+          |{' '}
+          <span className="category-link" onClick={() => this.filterCategory('Soap')}>
+            SOAP
+          </span>{' '}
+          |{' '}
+          <span className="category-link" onClick={() => this.filterCategory('Cologne')}>
+            COLOGNE
+          </span>{' '}
+          |{' '}
+          <span className="category-link" onClick={() => this.filterCategory('Shave')}>
+            SHAVE
+          </span>{' '}
+          |{' '}
+          <span className="category-link" onClick={() => this.filterCategory('Tactical')}>
+            TACTICAL
+          </span>
+        </nav>
+        <div>
+          {items.map((item, index) => {
+            return <Product key={index} item={item} add={add} />;
+          })}
+        </div>
+      </section>
+    );
+  }
+}
 
 Shop.propTypes = {
   items: PropTypes.array,
-  add: PropTypes.func
+  add: PropTypes.func,
 };
 
 export default Shop;
