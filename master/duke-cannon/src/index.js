e93924c26e842dfc19f61e16a66a3313413fe96d
Brad Murphy <brad.murphy@Brads-MacBook-Pro.local>
2018-12-07 14:44:06 -0500

    Uploading prod fix.

--- master/duke-cannon/src/index.js
+++ master/duke-cannon/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.scss';
+import './index.scss';
 import App from './containers/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -13,7 +13,7 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 registerServiceWorker();
